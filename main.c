c57668f40be413535e13eb3cb82c9eb674e31f0a
Esrog <gorse.sacha@gmail.com>
2020-10-22 15:08:38 +0200

    Ajout de l'initialisation de SDL + Terrain.bmp

--- main.c
+++ main.c
@@ -1,38 +1,81 @@
-#include <SDL2/SDL.h>
-#include <stdio.h>
-//https://github.com/Esr0g/ProjetL2Info.git
+#include "main.h"
+/* https://github.com/Esr0g/ProjetL2Info.git */
 
-int main(int argc, char** argv)
+int main(int argc, char *argv[])
 {
-    /* Initialisation simple */
-    if (SDL_Init(SDL_INIT_VIDEO) != 0 )
-    {
-        fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
-        return -1;
+    SDL_Window *pWindow = NULL;
+    SDL_Renderer *pRenderer = NULL;
+
+    if (SDL_Init (SDL_INIT_VIDEO) != 0) {
+        SDL_Log ("ERREUR: Initialisation SDL > %s\n", SDL_GetError());
+
+        exit(EXIT_FAILURE);
     }
 
-    {
-        /* Création de la fenêtre */
-        SDL_Window* pWindow = NULL;
-        pWindow = SDL_CreateWindow("Ma première application SDL2",SDL_WINDOWPOS_UNDEFINED,
-                                                                  SDL_WINDOWPOS_UNDEFINED,
-                                                                  1152,
-                                                                  960,
-                                                                  SDL_WINDOW_SHOWN);
+    pWindow = SDL_CreateWindow("Tower Defense", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, FENETRE_LARGEUR, FENETRE_HAUTEUR, SDL_WINDOW_SHOWN);
+    if (pWindow == NULL) {
+        SDL_Log("ERREUR: Creation de la fenêtre > %s\n", SDL_GetError());
 
-        if( pWindow )
-        {
-            SDL_Delay(6000); /* Attendre trois secondes, que l'utilisateur voie la fenêtre */
-
-            SDL_DestroyWindow(pWindow);
-        }
-        else
-        {
-            fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
-        }
+        SDL_Quit();
+        exit(EXIT_FAILURE);
     }
 
+    pRenderer = SDL_CreateRenderer (pWindow, -1, SDL_RENDERER_ACCELERATED);
+    if (pRenderer == NULL) {
+        SDL_Log("ERREUR: Creation du Rendu > %s\n", SDL_GetError());
+
+        SDL_DestroyWindow (pWindow);
+        SDL_Quit();
+        exit(EXIT_FAILURE);
+    }
+
+    SDL_Surface *image = SDL_LoadBMP ("textures/Terrain.bmp");
+    if (image == NULL) {
+        SDL_Log("ERREUR: Chargement de l'image > %s\n", SDL_GetError());
+
+        SDL_DestroyRenderer(pRenderer);
+        SDL_DestroyWindow (pWindow);
+        SDL_Quit();
+        exit(EXIT_FAILURE);
+    }
+
+    SDL_Texture *pTextureImage = SDL_CreateTextureFromSurface(pRenderer, image);
+    if (pTextureImage == NULL) {
+        SDL_Log("ERREUR: Creation du rendu à partir d'une image > %s\n", SDL_GetError());
+
+        SDL_FreeSurface(image);
+        SDL_DestroyRenderer(pRenderer);
+        SDL_DestroyWindow (pWindow);
+        SDL_Quit();
+        exit(EXIT_FAILURE);
+    }
+
+    SDL_FreeSurface(image);
+
+    SDL_bool program_launched = SDL_TRUE;
+
+    while (program_launched) {
+        SDL_Event events;
+
+        while (SDL_PollEvent(&events)) {
+            switch (events.type) {
+                case SDL_QUIT:
+                    program_launched = SDL_FALSE;
+                    break;
+                default:
+                    continue;
+            }
+        }
+
+        SDL_RenderCopy(pRenderer, pTextureImage, NULL, NULL);
+
+        SDL_RenderPresent (pRenderer);
+    }
+
+    SDL_DestroyTexture(pTextureImage);
+    SDL_DestroyWindow (pWindow);
+    SDL_DestroyRenderer (pRenderer);
     SDL_Quit();
 
-    return 0;
+    return EXIT_SUCCESS;
 }
---
+++ main.h
@@ -0,0 +1,6 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define FENETRE_LARGEUR 1152
+#define FENETRE_HAUTEUR 960
