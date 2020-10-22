bc2d60245664f75c3ff2471f5466cedb0e6907af
ChattonDonovan <chatton.donovan@gmail.com>
2020-10-22 15:44:12 +0200

    Ajout Fichier jeu.c

---
+++ jeu.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+typedef struct Ennemis1 Ennemis1;
+struct Ennemis1
+{
+    int vie; 
+	
+	
+	int getVie(void)
+	{
+		return vie;
+	}		
+};
+
+
+typedef struct Base Base;
+struct Base
+{
+
+};
